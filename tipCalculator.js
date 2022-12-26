89f90c65c2dbc813840b6df14042f972ed7dad04
1706Priyanka <116390351+1706Priyanka@users.noreply.github.com>
2022-12-26 19:02:12 +0530

    Add files via upload

---
+++ tipCalculator.js
@@ -0,0 +1,7 @@
+const amount = document.getElementById("bill");
+const numOfPeople = document.getElementById("noOfPeople");
+const customPer = document.getElementById("customTip");
+const billTip = document.getElementById("tipAmount");
+const billPerPerson = document.getElementById("perPersonTotal");
+const resetButton = document.getElementById("reset");
+const tipButton = document.querySelectorAll(".tipButtons button");
